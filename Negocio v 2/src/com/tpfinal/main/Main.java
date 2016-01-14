1e0161f0a4880694e520f7db35e8ce6a5bf4c88e
Desiree Fauda <desireefauda@gmail.com>
2016-01-13 22:13:15 -0300

    Prueba de commit

--- Negocio v 2/src/com/tpfinal/main/Main.java
+++ Negocio v 2/src/com/tpfinal/main/Main.java
@@ -14,7 +14,7 @@ import org.hibernate.cfg.Configuration;
 public class Main {
 
     public static void main(String[] args) {
-//        System.out.println("---------Setting up Hibernate --------");
+//        System.out.println("HOLA MUNDOOOOO---------Setting up Hibernate --------");
 //        Configuration cfg = new Configuration();
 //        cfg.configure("com.tpfinal.config/hibernate.cfg.xml");
 //
